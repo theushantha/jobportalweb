413c258b887c29c068801cbc20f4a2ae552a748b
Ushantha <ushathagreat@gmail.com>
2026-01-11 17:21:01 +0530

    Created profiles for Recruit and Job Seekers

---
+++ src/main/java/com/cash2code/jobportal/entity/RecruiterProfile.java
@@ -0,0 +1,127 @@
+package com.cash2code.jobportal.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recruiter_profile")
+public class RecruiterProfile {
+
+    @Id
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    @MapsId
+    private Users userId;
+
+    private String firstname;
+    private String lastname;
+    private String city;
+    private String state;
+    private String country;
+    private String company;
+    @Column(nullable = true, length = 64)
+    private String profilePhoto;
+
+    public RecruiterProfile() {
+    }
+
+    public RecruiterProfile(int userAccountId, Users userId, String firstname, String lastname, String city, String state, String country, String company, String profilePhoto) {
+        this.userAccountId = userAccountId;
+        this.userId = userId;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.company = company;
+        this.profilePhoto = profilePhoto;
+    }
+
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    @Override
+    public String toString() {
+        return "RecruiterProfile{" +
+                "userAccountId=" + userAccountId +
+                ", userId=" + userId +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", company='" + company + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/cash2code/jobportal/entity/Skills.java
@@ -0,0 +1,9 @@
+package com.cash2code.jobportal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "skills")
+public class Skills {
+}
