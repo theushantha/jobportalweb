4c791461a02bc8c7002bad1378296cbd6cbbda27
Ushantha <ushathagreat@gmail.com>
2026-01-13 01:03:59 +0530

    Security Config

---
+++ src/main/java/com/cash2code/jobportal/services/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.cash2code.jobportal.services;
+
+import com.cash2code.jobportal.entity.Users;
+import com.cash2code.jobportal.repository.UsersRepository;
+import com.cash2code.jobportal.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not found user"));
+        return new CustomUserDetails(user);
+    }
+}
