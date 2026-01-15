45a760f091dc97d7617a3657ec36b1a1a93600ac
Ushantha <ushathagreat@gmail.com>
2026-01-15 23:23:16 +0530

    Display the jobs added by Recruiter

--- src/main/java/com/cash2code/jobportal/controller/JobpostActivityController.java
+++ src/main/java/com/cash2code/jobportal/controller/JobpostActivityController.java
@@ -1,12 +1,15 @@
 package com.cash2code.jobportal.controller;
 
 import com.cash2code.jobportal.entity.JobPostActivity;
+import com.cash2code.jobportal.entity.RecruiterJobsDto;
+import com.cash2code.jobportal.entity.RecruiterProfile;
 import com.cash2code.jobportal.entity.Users;
 import com.cash2code.jobportal.services.JobPostActivityService;
 import com.cash2code.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobpostActivityController {
@@ -36,6 +40,11 @@ public class JobpostActivityController {
         if (!(authentication instanceof AnonymousAuthenticationToken)){
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile)
+                        currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost",recruiterJobs);
+            }
         }
         model.addAttribute("user", currentUserProfile);
         System.out.println("Dashboard");
